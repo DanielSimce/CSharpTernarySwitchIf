7ad040eccf7f50f8d8f6b0f1a17647f91b4ed64c
Daniel <daniel.simonovski24@gmail.com>
2021-03-17 16:00:03 +0100

    start

---
+++ Switch/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Switch();
+            IfElse(18);
+
+            int myInt = 24;
+
+            string result = (myInt >= 24) ? "greater than 24" : "less than 24";
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+
+
+        public static void Switch()
+        {
+            int day = 8;
+
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+
+                default:
+                    Console.WriteLine("Default!");
+                    break;
+
+
+            }
+        }
+
+        public static void IfElse(int age)
+        {
+            if (age >= 18)
+            {
+                Console.WriteLine("Adult");
+            }
+            else
+            {
+                Console.WriteLine("Kid");
+            }
+        }
+
+
+    }
+}
